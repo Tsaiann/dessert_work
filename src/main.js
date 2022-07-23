62878ffa127237ac751a2c865dba168cf90e45a4
ann <ann110149@gmail.com>
2022-07-23 14:11:31 +0800

    update primeVue component

--- src/main.js
+++ src/main.js
@@ -2,18 +2,55 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import request from '@/api/request'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import PrimeVue from 'primevue/config'
-import Dialog from 'primevue/dialog'
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
 import '@/styles/style.scss'
 
+import Dialog from 'primevue/dialog'
+import Toast from 'primevue/toast'
+import Message from 'primevue/message'
+import ToastService from 'primevue/toastservice'
+import InputText from 'primevue/inputtext'
+import Calendar from 'primevue/calendar'
+import Button from 'primevue/button'
+import Breadcrumb from 'primevue/breadcrumb'
+import Checkbox from 'primevue/checkbox'
+import RadioButton from 'primevue/radiobutton'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Row from 'primevue/row'
+import Paginator from 'primevue/paginator'
+import Menubar from 'primevue/menubar'
+import Steps from 'primevue/steps'
+import Dropdown from 'primevue/dropdown'
+import Textarea from 'primevue/textarea'
+
 const app = createApp(App)
-//app.config.globalProperties.$request = request
 app.use(store)
 app.use(router)
 app.use(VueAxios, axios)
 app.use(PrimeVue)
+app.component('Menubar', Menubar)
+app.component('Toast', Toast)
+app.component('Textarea', Textarea)
+app.component('Dropdown', Dropdown)
+app.component('Steps', Steps)
+app.component('InputText', InputText)
+app.component('Breadcrumb', Breadcrumb)
+app.component('RadioButton', RadioButton)
+app.component('Row', Row)
+app.component('Paginator', Paginator)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('Checkbox', Checkbox)
+app.component('Calendar', Calendar)
 app.component('Dialog', Dialog)
+app.component('Message', Message)
+app.component('Button', Button)
+app.use(ToastService)
 app.mount('#app')
