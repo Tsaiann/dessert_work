4bd9e8e5d1fe83b092dd6e1e18fda3ee3a6d2946
ann <ann110149@gmail.com>
2022-12-06 14:35:55 +0800

    fix: about style

--- src/components/navigator/about.vue
+++ src/components/navigator/about.vue
@@ -9,7 +9,7 @@
           <img src="../../assets/other/about.png" alt="about" data-space-right="3rem" />
           <img src="../../assets/other/about_word.png" alt="about" data-space-right="1rem" />
           <div class="about-content-text">
-            <span data-space-bottom="1rem">季菓 Season Fructification</span>
+            <h2 data-space-bottom="1rem">季菓 Season Fructification</h2>
             <p>意味著由每個季節所帶來不同風味的新鮮水果，結合各式甜點創造出只屬於當季風味的特色甜點。</p>
             <p>使用最簡單細緻的方式烘培，讓顧客除了可以吃到食物本身的風味外，也能感受到其與甜點融合後的另一種層次口感。</p>
             <p>堅持不添加任何加工食品，希望能讓顧客消費時感受到家一樣的溫暖與安心。</p>
