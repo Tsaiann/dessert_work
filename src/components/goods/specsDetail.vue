dc26f16025383682f6fe5e918546017a6340618c
ann <ann110149@gmail.com>
2022-12-07 14:53:06 +0800

    fix: fix specsDetail style

--- src/components/goods/specsDetail.vue
+++ src/components/goods/specsDetail.vue
@@ -1,107 +1,108 @@
 <template>
   <div class="container products">
     <guideLine :data="guideData" />
-    <div class="goods-main">
-      <div class="goods-main-above">
-        <div data-width="70%" class="goods_galleria">
-          <Galleria :value="state.imgList" :numVisible="state.imgList.length">
-            <template #item="slotProps">
-              <img :src="slotProps.item.url" :alt="slotProps.item.Ident" />
-            </template>
-            <template #thumbnail="slotProps">
-              <img :src="slotProps.item.url" :alt="slotProps.item.Ident" />
-            </template>
-          </Galleria>
-        </div>
-        <div class="goods-main-content">
-          <h2>{{ state.goodsData.Name }}</h2>
-          <hr />
-          <p>建議售價 NT{{ state.goodsData.UnitPrice }}</p>
-          <div class="specs_exception">
-            <div class="row horizontal bottom_left" data-space-bottom="1.5rem">
-              <h3 data-space-right="1rem">選擇規格</h3>
-              <span>( 最多可選{{ state.specsCount }}種 )</span>
+    <div class="detail">
+      <div class="goods-choose">
+        <div class="goods-choose-above">
+          <div data-width="70%" class="galleria">
+            <Galleria :value="state.imgList" :numVisible="state.imgList.length">
+              <template #item="slotProps">
+                <img :src="slotProps.item.url" :alt="slotProps.item.Ident" />
+              </template>
+              <template #thumbnail="slotProps">
+                <img :src="slotProps.item.url" :alt="slotProps.item.Ident" />
+              </template>
+            </Galleria>
+          </div>
+          <div class="goods-choose-content">
+            <h2>{{ state.goodsData.Name }}</h2>
+            <hr />
+            <p>建議售價 NT{{ state.goodsData.UnitPrice }}</p>
+            <div class="specs_exception">
+              <div class="row horizontal bottom_left" data-space-bottom="1.5rem">
+                <h3 data-space-right="1rem">選擇規格</h3>
+                <span>( 最多可選{{ state.specsCount }}種 )</span>
+              </div>
+              <div class="field-checkbox">
+                <div v-for="(item, i) in state.newGoodsData" :key="i" class="field-checkbox-item">
+                  <input :id="item.ID" type="checkbox" name="specs" :value="item.ID" v-model="state.allSpecsCheckedID" @click="handleSpecsMax($event)" />
+                  <label :for="item.ID">{{ item.Specs }}：</label>
+                  <input class="specsCount" type="number" v-model="item.Num" max="99" min="1" placeholder="數量" />
+                </div>
+              </div>
             </div>
-            <div class="field-checkbox">
-              <div v-for="(item, i) in state.newGoodsData" :key="i" class="field-checkbox-item">
-                <input :id="item.ID" type="checkbox" name="specs" :value="item.ID" v-model="state.allSpecsCheckedID" @click="handleSpecsMax($event)" />
-                <label :for="item.ID">{{ item.Specs }}：</label>
-                <input class="specsCount" type="number" v-model="item.Num" max="99" min="1" placeholder="數量" />
+            <div>
+              <h3>購買數量</h3>
+              <div class="goods-choose-content__count" data-space-left="1.5rem">
+                <i class="pi pi-minus" @click="countChange('minus')"></i>
+                <InputNumber class="p-inputtext-sm" v-model="state.count" />
+                <i class="pi pi-plus" style="color: red" @click="countChange('plus')"></i>
+              </div>
+              <div class="like" v-if="state.like === false" @click="handleAddLike">
+                <i class="pi pi-heart" data-space-left="1rem"></i>
+                <span data-space-left="0.5rem">加入到收藏清單</span>
+              </div>
+              <div class="like_finish" v-else>
+                <i class="pi pi-heart-fill" data-space-left="1rem"></i>
+                <span data-space-left="0.5rem">已加入收藏清單</span>
               </div>
             </div>
           </div>
-          <div>
-            <h3>購買數量</h3>
-            <div class="goods-main-content__count" data-space-left="1.5rem">
-              <i class="pi pi-minus" @click="countChange('minus')"></i>
-              <InputNumber class="p-inputtext-sm" v-model="state.count" />
-              <i class="pi pi-plus" style="color: red" @click="countChange('plus')"></i>
-            </div>
-            <div class="like" v-if="state.like === false" @click="handleAddLike">
-              <i class="pi pi-heart" data-space-left="1rem"></i>
-              <span data-space-left="0.5rem">加入到收藏清單</span>
-            </div>
-            <div class="like_finish" v-else>
-              <i class="pi pi-heart-fill" data-space-left="1rem"></i>
-              <span data-space-left="0.5rem">已加入收藏清單</span>
-            </div>
+        </div>
+        <div class="button_buy media">
+          <button class="button_normal" data-width="50%" @click="addCart()">加入購物車</button>
+          <button class="button_confirm" data-width="50%" @click="handleBuy()">直接購買</button>
+        </div>
+        <div class="button_buy">
+          <button class="button_normal" data-width="50%" @click="addCart()">加入購物車</button>
+          <button class="button_confirm" data-width="50%" @click="handleBuy()">直接購買</button>
+        </div>
+      </div>
+      <div class="goods-data">
+        <div class="row horizontal h_center">
+          <div data-width="50%" class="row vertical center">
+            <h1 data-width="100%">商品詳細資訊</h1>
+            <div class="color-block"></div>
           </div>
-        </div>
-      </div>
-      <div class="button_buy media">
-        <button class="button_submit normal" data-width="50%" @click="addCart()">加入購物車</button>
-        <button class="button_submit confirm" data-width="50%" @click="handleBuy()">直接購買</button>
-      </div>
-      <div class="button_buy">
-        <button class="button_submit normal" data-width="50%" @click="addCart()">加入購物車</button>
-        <button class="button_submit confirm" data-width="50%" @click="handleBuy()">直接購買</button>
-      </div>
-    </div>
-    <div class="goods-data">
-      <div class="row horizontal h_center">
-        <div data-width="50%" class="row vertical center">
-          <h1 data-width="100%">商品詳細資訊</h1>
-          <div class="color-block"></div>
-        </div>
-        <h1 data-width="50%" @click="$router.push({ name: 'Delivery' })">送貨及付款方式</h1>
-      </div>
-      <div class="wire"></div>
-      <h2>商品詳細資訊</h2>
-      <p style="white-space: pre-wrap">{{ state.goodsData.Description }}</p>
-    </div>
-    <div class="goods-about">
-      <div>
-        <div data-width="100%" class="row vertical center">
-          <h1 data-width="100%">相關產品</h1>
-          <div class="color-block"></div>
+          <h1 data-width="50%" @click="$router.push({ name: 'Delivery' })">送貨及付款方式</h1>
         </div>
         <div class="wire"></div>
+        <h2>商品詳細資訊</h2>
+        <p style="white-space: pre-wrap">{{ state.goodsData.Description }}</p>
       </div>
-      <div class="carousel">
-        <Carousel :value="goodsList" :numVisible="4" :responsiveOptions="responsiveOptions">
-          <template #item="slotProps">
-            <div class="product-item" @click="aboutGoodsRouter(slotProps.data.id)">
-              <div class="product-item-content">
-                <div>
-                  <img :src="slotProps.data.src" alt="" />
-                </div>
-                <div>
-                  <h4>{{ slotProps.data.name }}</h4>
-                  <h6>NT {{ slotProps.data.price }}</h6>
+      <div class="goods-about">
+        <div>
+          <div data-width="100%" class="row vertical center">
+            <h1 data-width="100%">相關產品</h1>
+            <div class="color-block"></div>
+          </div>
+          <div class="wire"></div>
+        </div>
+        <div class="carousel">
+          <Carousel :value="goodsList" :numVisible="4" :responsiveOptions="responsiveOptions">
+            <template #item="slotProps">
+              <div class="goods-item" @click="aboutGoodsRouter(slotProps.data.id)">
+                <div class="goods-item-content">
+                  <div>
+                    <img :src="slotProps.data.src" alt="" />
+                  </div>
+                  <div>
+                    <h4>{{ slotProps.data.name }}</h4>
+                    <h6>NT {{ slotProps.data.price }}</h6>
+                  </div>
                 </div>
               </div>
-            </div>
-          </template>
-        </Carousel>
+            </template>
+          </Carousel>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import { reactive, onMounted, inject, computed, ref } from 'vue'
+import { reactive, onMounted, ref } from 'vue'
 import { callApi } from '@/utils/callApi'
-import { useStore } from 'vuex'
-import { addGoodsCart, addLikeList, allLikeList, getImg, getGoodsList } from '@/service/api'
+import { addGoodsCart, addLikeList, allLikeList, getGoodsList } from '@/service/api'
 import { useToast } from 'primevue/usetoast'
 import { useRouter } from 'vue-router'
 import guideLine from '@/components/guideLine.vue'
@@ -112,10 +113,10 @@ export default {
     guideLine
   },
   setup() {
-    const reload = inject('reload')
     const toast = useToast()
     const router = useRouter()
     const state = reactive({
+      memberInfo: localStorage.getItem('memberInfo'),
       goodsData: [],
       newGoodsData: [],
       imgList: [],
@@ -130,6 +131,23 @@ export default {
       },
       like: false
     })
+    const responsiveOptions = ref([
+      {
+        breakpoint: '900px',
+        numVisible: 3,
+        numScroll: 2
+      },
+      {
+        breakpoint: '700px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '500px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ])
     const goodsList = reactive([
       {
         id: 15,
@@ -156,6 +174,12 @@ export default {
         src: require('../../assets/goods/strawberryChiffon01.jpg')
       }
     ])
+    const guideData = reactive([
+      {
+        label: null,
+        to: '/goodsDetail'
+      }
+    ])
     //獲得該商品詳細資料
     const getGoodDetail = onMounted(() => {
       const data = { ID: localStorage.getItem('goodsDetailID') * 1 }
@@ -176,12 +200,6 @@ export default {
         state.imgList.push({ url: process.env.VUE_APP_BASE_API + '/imgs/' + url })
       }
     }
-    const guideData = reactive([
-      {
-        label: null,
-        to: '/goodsDetail'
-      }
-    ])
     //需要的商品數量
     const countChange = (name) => {
       if (name === 'plus' && state.count < 99) {
@@ -192,8 +210,7 @@ export default {
     }
     //加入到購物車
     const addCart = async () => {
-      const memberInfo = localStorage.getItem('memberInfo')
-      if (memberInfo !== null) {
+      if (state.memberInfo !== null) {
         checkSpecs()
         const totalNum = state.cartForm.Specs.reduce((pre, cur) => {
           return pre + cur.Num
@@ -205,7 +222,7 @@ export default {
           })
           const data = state.cartForm
           callApi(addGoodsCart, data, () => {
-            toast.add({ severity: 'success', summary: '已加入購物車！', group: 'goods_addcart' })
+            toast.add({ severity: 'success', summary: '已加入購物車！', group: 'successBox' })
             state.cartForm.Specs = []
           })
         } else {
@@ -220,8 +237,7 @@ export default {
     }
     //直接購買（跳轉結帳）
     const handleBuy = async () => {
-      const memberInfo = localStorage.getItem('memberInfo')
-      if (memberInfo !== null) {
+      if (state.memberInfo !== null) {
         checkSpecs()
         const totalNum = state.cartForm.Specs.reduce((pre, cur) => {
           return pre + cur.Num
@@ -246,8 +262,7 @@ export default {
     }
     //加入到收藏清單
     const handleAddLike = () => {
-      const memberInfo = localStorage.getItem('memberInfo')
-      if (memberInfo !== null) {
+      if (state.memberInfo !== null) {
         const data = { GoodsID: state.cartForm.GoodsID }
         callApi(addLikeList, data, () => {
           state.like = true
@@ -258,13 +273,9 @@ export default {
     }
     //判斷收藏清單裡是否已有資料
     const likeStatus = () => {
-      const memberInfo = localStorage.getItem('memberInfo')
-      if (memberInfo === null) {
-        return
-      } else {
+      if (state.memberInfo !== null) {
         const data = ''
         callApi(allLikeList, data, (res) => {
-          console.log(res)
           for (let i in res.data.Data) {
             if (res.data.Data[i].GoodsID === state.cartForm.GoodsID) {
               state.like = true
@@ -283,7 +294,6 @@ export default {
       if (event.target.checked === true) {
         if (state.currentSpecsChecked.length < state.goodsData.GoodsSpecs[0].Specs) {
           state.currentSpecsChecked.push(event.target.value)
-          console.log(state.currentSpecsChecked)
         } else {
           event.target.checked = false
           toast.add({ severity: 'error', summary: '已超過數量！', group: 'errorBox' })
@@ -292,9 +302,9 @@ export default {
         state.currentSpecsChecked = state.currentSpecsChecked.filter((item) => {
           return item !== event.target.value
         })
-        console.log(state.currentSpecsChecked)
       }
     }
+    //檢查商品數量是否正確
     const checkSpecs = () => {
       for (let i in state.goodsData.GoodsSpecs) {
         for (let j in state.allSpecsCheckedID) {
@@ -307,41 +317,19 @@ export default {
         }
       }
     }
-    const responsiveOptions = ref([
-      {
-        breakpoint: '900px',
-        numVisible: 3,
-        numScroll: 2
-      },
-      {
-        breakpoint: '700px',
-        numVisible: 2,
-        numScroll: 2
-      },
-      {
-        breakpoint: '500px',
-        numVisible: 1,
-        numScroll: 1
-      }
-    ])
-    const test = (id) => {
-      console.log(id)
-    }
     return {
-      test,
-      responsiveOptions,
       state,
+      responsiveOptions,
       getGoodDetail,
       guideData,
+      goodsList,
       likeStatus,
       handleAddLike,
       handleBuy,
       addCart,
       countChange,
       aboutGoodsRouter,
-      handleSpecsMax,
-      checkSpecs,
-      goodsList
+      handleSpecsMax
     }
   }
 }
